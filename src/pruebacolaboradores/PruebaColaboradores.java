0ec4e24ce981b3531413ef15f65ff67cc8d49c42
Aralvarellosblas <arturo.alvarellos@gmail.com>
2017-10-20 08:47:30 +0200

    Pisto para subir, pueba de colaboradores

---
+++ src/pruebacolaboradores/PruebaColaboradores.java
@@ -0,0 +1,11 @@
+package pruebacolaboradores;
+/**
+ * @author Arturo
+ */
+public class PruebaColaboradores {
+
+    public static void main(String[] args) {
+        // Listo para la prueba de colaboración.
+    }
+    
+}
