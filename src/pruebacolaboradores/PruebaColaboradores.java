d7b34d4144e6718aa2a8dae2f9908293a9361287
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2017-10-20 08:52:30 +0200

    Prueba de clonado realizada con exito.

--- src/pruebacolaboradores/PruebaColaboradores.java
+++ src/pruebacolaboradores/PruebaColaboradores.java
@@ -6,6 +6,7 @@ public class PruebaColaboradores {
 
     public static void main(String[] args) {
         // Listo para la prueba de colaboración.
+        //Prueba de clonado realizada Juan.
     }
     
 }
