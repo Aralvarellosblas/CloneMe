faf509e176d1f32f0715b69eac0fa1265ec2d3ab
Aralvarellosblas <arturo.alvarellos@gmail.com>
2017-10-20 08:58:44 +0200

    Prueba realizada con éxito

--- src/pruebacolaboradores/PruebaColaboradores.java
+++ src/pruebacolaboradores/PruebaColaboradores.java
@@ -7,6 +7,7 @@ public class PruebaColaboradores {
     public static void main(String[] args) {
         // Listo para la prueba de colaboración.
         //Prueba de clonado realizada Juan.
+        //Prueba correcta.
     }
     
 }
